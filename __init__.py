11748af4f3c9d605c606c814debe88a588acf67f
cheminduroi <thakid340@gmail.com>
2019-09-27 23:22:47 -0400

    update imports

--- __init__.py
+++ __init__.py
@@ -1,8 +1,6 @@
 import argparse
 import pickle
 import os.path
-import requests
-from oauth2client import file, client, tools
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
